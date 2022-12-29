9a5e5b0aa674b53fe2653d82280e2794dbf2734e
hector7178 <hjgrimanmarcano@gmail.com>
2022-12-29 19:38:28 -0400

    new

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+
+
 const nextConfig = {
   reactStrictMode: true,
+ 
+ 
+  
+  assetPrefix:  'https://cdn.widget-ashy.vercel.app' ,
 }
 
 module.exports = nextConfig
